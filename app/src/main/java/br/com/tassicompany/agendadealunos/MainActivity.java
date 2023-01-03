92e9778b9dfee6ccbe3bdeb2da2363a832f0f5f9
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-01-02 22:53:55 -0300

    Overwrite onCreate method and show a Toast message

--- app/src/main/java/br/com/tassicompany/agendadealunos/MainActivity.java
+++ app/src/main/java/br/com/tassicompany/agendadealunos/MainActivity.java
@@ -1,7 +1,18 @@
 package br.com.tassicompany.agendadealunos;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show();
+    }
 }
