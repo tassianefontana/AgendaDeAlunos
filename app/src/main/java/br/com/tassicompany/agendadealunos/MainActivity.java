a6d3bd359200387c77db30eec2da37f84e91955c
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-01-03 22:30:45 -0300

    Create array list with some students

--- app/src/main/java/br/com/tassicompany/agendadealunos/MainActivity.java
+++ app/src/main/java/br/com/tassicompany/agendadealunos/MainActivity.java
@@ -8,12 +8,29 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Tassi", "Marcia", "Carol"));
+        TextView aluno1 = findViewById(R.id.tvAluno1);
+        TextView aluno2 = findViewById(R.id.tvAluno2);
+        TextView aluno3 = findViewById(R.id.tvAluno3);
+
+        aluno1.setText(alunos.get(0));
+        aluno2.setText(alunos.get(1));
+        aluno3.setText(alunos.get(2));
+
+
 
     }
 }
