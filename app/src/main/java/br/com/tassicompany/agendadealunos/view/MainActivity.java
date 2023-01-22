2198156962fd7d7615a651d03c84a14160c8dd3f
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-01-21 22:20:21 -0300

    Create Context Menu using XML

--- app/src/main/java/br/com/tassicompany/agendadealunos/view/MainActivity.java
+++ app/src/main/java/br/com/tassicompany/agendadealunos/view/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        menu.add("Remover");
+        getMenuInflater().inflate(R.menu.activity_lista_alunos_menu, menu);
     }
 
     @Override
