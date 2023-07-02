8c6176afd4bb66f1cd6486b1d6579ba6c05fca57
Tassiane Sayuri Fontana <tassiane.fontana@gmail.com>
2023-07-02 00:23:05 -0300

    Implement migrations

--- app/src/main/java/br/com/tassicompany/agendadealunos/database/AgendaDatabase.java
+++ app/src/main/java/br/com/tassicompany/agendadealunos/database/AgendaDatabase.java
@@ -2,21 +2,30 @@ package br.com.tassicompany.agendadealunos.database;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import br.com.tassicompany.agendadealunos.database.dao.AlunoDAO;
 import br.com.tassicompany.agendadealunos.model.Aluno;
 
-@Database(entities = {Aluno.class}, version = 1, exportSchema = false)
+@Database(entities = {Aluno.class}, version = 2, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
     private static final String NOME_BANCO_DE_DADOS ="agenda.db";
 
     public static AgendaDatabase getInstance(Context context) {
         return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
                 .allowMainThreadQueries()
-                .fallbackToDestructiveMigration() //Limpa os dados da versão anterior do banco de dados.
+      //          .fallbackToDestructiveMigration() //Limpa os dados da versão anterior do banco de dados.
+                .addMigrations(new Migration(1, 2) {
+                    @Override
+                    public void migrate(@NonNull SupportSQLiteDatabase database) {
+                        database.execSQL("ALTER TABLE aluno ADD COLUMN sobrenome TEXT");
+                    }
+                })
                 .build();
     }
 
